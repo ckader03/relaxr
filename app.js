46fd082ba523e5f5e7bb9703fac0b54ec6144a43
ckader03 <ckader065@gmail.com>
2019-11-18 20:39:31 -0500

    add changes

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app     = express();
+const mustacheExpress = require('mustache-express');
+const port    = process.env.PORT || 8000;
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`)
+});
